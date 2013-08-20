2d2c3046103947aa2ec227e53e52f3eacb14df5a
joest <chaojiong.zheng@gmail.com>
2013-08-20 16:52:18 +0800

    add weibo follow

--- sidebar.php
+++ sidebar.php
@@ -13,6 +13,10 @@ $current_layout = $options['theme_layout'];
 if ( 'content' != $current_layout ) :
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
+		<aside id="archives" class="widget">
+		    <h3 class="widget-title">关注是一种态度</h3>
+		<iframe width="100%" height="67" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=2&ptype=1&speed=0&skin=1&isTitle=0&noborder=0&isWeibo=0&isFans=0&uid=1889101803&verifier=06903df6&colors=d6f3f7,ffffff,666666,1b8be0,ecfbfd&dpc=1"></iframe>
+		</aside>
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 				<aside id="archives" class="widget">
