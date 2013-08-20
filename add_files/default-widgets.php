5ef4033cd5c82060a5daf9a5a4bd947e5a32663c
joest <chaojiong.zheng@gmail.com>
2013-08-20 15:46:32 +0800

    add Travle_Widget widget.

--- add_files/default-widgets.php
+++ add_files/default-widgets.php
@@ -1156,6 +1156,87 @@ class WP_Widget_Tag_Cloud extends WP_Widget {
 	}
 }
 
+/**
+ * Adds Travel_Widget widget.
+ */
+class Travel_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'travel_widget', // Base ID
+			'Travel_Widget', // Name
+			array( 'description' => __( 'Posts about Travel  Widget', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$travel_posts = new WP_Query( array( 'category_name' => 'travel', 'orderby' => 'title', 'order' => 'DESC' ) ); 
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+		while ($travel_posts->have_posts()) : $travel_posts->the_post();
+		echo '<a href="';
+                echo the_permalink();
+		echo '">';
+                echo the_title();
+		echo '</a><br/>';
+                endwhile;
+
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title', 'text_domain' );
+		}
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // class Travel_Widget
 
 
 /**
@@ -1197,7 +1278,7 @@ function wp_widgets_init() {
 
 	register_widget('WP_Nav_Menu_Widget');
 
-    register_widget( 'Foo_Widget' );
+    register_widget( 'Travel_Widget' );
 
 	do_action('widgets_init');
 }
